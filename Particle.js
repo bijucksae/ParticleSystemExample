1735be6a7ad29e0201ebf9ed7d2c2f7cc2972987
bijucksae <jiwonmin000@gmail.com>
2024-11-12 16:11:36 +0900

    파티클의 사이즈를 변수화

--- Particle.js
+++ Particle.js
@@ -2,11 +2,11 @@
 
 class Particle {
     constructor(position) {
-        this.acceleration = createVector(0, 0.05);
         this.acceleration = createVector(0, 0);
         this.velocity = createVector(random(-1, 1), random(-1, 0));
         this.position = position.copy();
         this.lifespan = 255;
+        this.w = 2;
     }
 
     run() {
@@ -17,10 +17,12 @@ class Particle {
     applyForce(force) {
         this.acceleration.add(force);
     }
+
     update() {
         this.velocity.add(this.acceleration);
         this.position.add(this.velocity);
         this.lifespan -= 2;
+
         this.acceleration.set(0, 0);
     }
 
@@ -28,7 +30,7 @@ class Particle {
         stroke(200, this.lifespan);
         strokeWeight(2);
         fill(127, this.lifespan);
-        ellipse(this.position.x, this.position.y, 12, 12);
+        ellipse(this.position.x, this.position.y, this.w, this.w);
     }
 
     isDead() {
