87b129c4cd9630bbcdce1e359ebd217bd31f0300
bijucksae <jiwonmin000@gmail.com>
2024-11-12 15:36:50 +0900

    파티클 안에 있던 숨은 중력을 빼고, applyFoce 적용

--- Particle.js
+++ Particle.js
@@ -1,40 +1,37 @@
 // A simple Particle class
 
-// class Particle {
-//     constructor() {
-    class Particle {
-        constructor(position) {
-            this.acceleration = createVector(0, 0.05);
-            this.velocity = createVector(random(-1, 1), random(-1, 0));
-            this.position = position.copy();
-            this.lifespan = 255;
-        }
-    
-    //     }
-        run() {
-            this.update();
-            this.display();
-        }
-    
-    //     run() {
-        update() {
-            this.velocity.add(this.acceleration);
-            this.position.add(this.velocity);
-            this.lifespan -= 2;
-        }
-    
-    //     }
-        display() {
-            stroke(200, this.lifespan);
-            strokeWeight(2);
-            fill(127, this.lifespan);
-            ellipse(this.position.x, this.position.y, 12, 12);
-        }
-    
-    //     update() {
-        isDead() {
-            return this.lifespan < 0;
-        }
+class Particle {
+    constructor(position) {
+        this.acceleration = createVector(0, 0.05);
+        this.acceleration = createVector(0, 0);
+        this.velocity = createVector(random(-1, 1), random(-1, 0));
+        this.position = position.copy();
+        this.lifespan = 255;
     }
-    
-    
+
+    run() {
+        this.update();
+        this.display();
+    }
+
+    applyForce(force) {
+        this.acceleration.add(force);
+    }
+    update() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.lifespan -= 2;
+        this.acceleration.set(0, 0);
+    }
+
+    display() {
+        stroke(200, this.lifespan);
+        strokeWeight(2);
+        fill(127, this.lifespan);
+        ellipse(this.position.x, this.position.y, 12, 12);
+    }
+
+    isDead() {
+        return this.lifespan < 0;
+    }
+}
